b621122f71e06e01af73e7fda016a9b9f6579327
chrisptran <116043845+chrisptran@users.noreply.github.com>
2023-08-15 01:15:00 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+//1. import dependencies
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+// 2. config
+require("./config/mongoose.config")
+
+app.use(cors())
+app.use(express.json(), express.urlencoded({ extended: true }))
+
+// 3. routes
+const Routes = require("./routes/destination.routes")
+Routes(app)
+
+// 4. listen to port
+app.listen(8000, () => console.log("listening to port 8000"))
